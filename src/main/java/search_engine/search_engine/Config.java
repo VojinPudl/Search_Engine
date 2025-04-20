8bc94e298e752c7881b90ccf0aea72cd1b25327b
VojiPudl <pudltomas007@gmail.com>
2025-04-20 19:58:47 +0200

    Search_Engine

---
+++ src/main/java/search_engine/search_engine/Config.java
@@ -0,0 +1,20 @@
+package search_engine.search_engine;
+
+import java.io.File;
+
+public class Config {
+    public int skin_var;
+    public String Path_var;
+
+    public Config() {
+    }
+
+    public int getSkin(File file){
+        return 0;
+    }
+
+    public String getPath(){
+        return "";
+    }
+
+}
